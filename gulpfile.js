7f1c3c0e1b41b6ca84eb620852ac988b2fd775b0
Piramanayagam R <piramanayagamr@syncfusion.com>
2017-12-29 11:48:54 +0530

    TS snippet corrected

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    merge = require('gulp-merge-json');
-
-gulp.task('ts-merge', function() {
-  gulp.src('typescript/snippets/**.json')
-      .pipe(merge({ fileName: 'index.json'}))
-      .pipe(gulp.dest('typescript/snippets'))
-});
